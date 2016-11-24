102ba5d3f55c6414822258832c4c818227b938af
Bert Heinzelman <heinzelmanb@gmail.com>
2016-11-24 14:14:03 -0800

    updated datamining lib (#1)

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from lib.Classifiers import DecisionTreeClassifier, KNNClassifier, BayesianClassifier
+from lib.Classifiers import DecisionTreeClassifier, KNNClassifier, BayesianClassifier, RandomForestClassifier
 from lib.Table import Table
 from lib.Test import CrossValidation
 import random
@@ -13,8 +13,12 @@ import random
 
 if __name__ == '__main__':
     CLASS_ID = 5
-    ATS = [0,1,2,3,4,6,7,8]
+    # ATS = [0,1,2,3,4,6,7,8]
+    ATS = [0,1,2,3,4,7]
+
+    print "Loading table..." 
     t = Table(file="datasets/shot_log.min.csv") 
+    print "Done...\n\n" 
 
     print 'Testing decision tree... '
     tree = DecisionTreeClassifier(CLASS_ID, ATS, t)
@@ -22,20 +26,31 @@ if __name__ == '__main__':
     test.run_test()
     print 'Done...'
 
+    print 'Testing Random Forest Classifier...' 
+    print 'Building Random Forest...'
+    rf = RandomForestClassifier(CLASS_ID, ATS, t, 15, 7, 5, domain=t.get_domain(ATS))
+    print 'Done...'
+
+    test = CrossValidation(rf, 4, t)
+    test.run_test()
+
     print "shuffling" 
     random.shuffle(t.table)
-    t.table = t.table[:2000]
+    t.table = t.table[:500]
     print "Done..."
 
-    print "testing... Bayes w/ dataset size 2000"
+    print "testing... Bayes w/ dataset size 500"
     bayes = BayesianClassifier(CLASS_ID, ATS, t)
-    test = CrossValidation(bayes, 4, t)
+    test = CrossValidation(bayes, 10, t)
     test.run_test()
     print 'Done...'
 
-    print "testing... KNN w/ dataset size 2000"
-    knnc = KNNClassifier(CLASS_ID, [], ATS, 5, t)
-    test = CrossValidation(knnc, 4, t)
+    print "testing... KNN w/ dataset size 500"
+    CONT_ATS = [2,3,4,7]
+    CAT_ATS = [0,1,6,8]
+
+    knnc = KNNClassifier(CLASS_ID, CONT_ATS, CAT_ATS, 5, t) 
+    test = CrossValidation(knnc, 10, t)
     test.run_test()
     print "Done..."
     
